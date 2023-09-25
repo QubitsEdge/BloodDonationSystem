bbea073fc06ac1f9ddd4201f82cee8f7002bf49e
chhas <hasan.sandhu.543@gmail.com>
2023-09-26 00:49:41 +0500

    After Commiting add-migration

---
+++ Model/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BloodDonationSystem.Model
+{
+    public class AppDbContext : DbContext
+    { 
+
+        public AppDbContext() : base()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var configSection = configBuilder.GetSection("ConnectionStrings");
+
+            var connectionString = configSection["BloodDonationConnection"];
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
